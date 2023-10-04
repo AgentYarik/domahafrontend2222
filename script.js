78745f700adfae182c8b7db9e10cb7d2003a5f27
AgentYarik <125293714+AgentYarik@users.noreply.github.com>
2023-10-04 21:42:29 +0300

    www

---
+++ script.js
@@ -0,0 +1,22 @@
+/* let result = 5 + 5 + '5';
+console.log(result); */
+
+/* let gmail = "agentyarikt7@gmail.com"; 
+
+let kikiki = gmail.includes('@');
+console.log(gmail.length()); */
+
+/* let a = "My";
+let b = "name";
+let c = "is";
+
+let fullName = a + " " + b + " " + c + " Yaroslav";
+
+console.log(fullName); */
+
+/* let name = "Олександро";
+let suma = 300;
+
+let povidos = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+
+alert(povidos); */
